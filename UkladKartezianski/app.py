ba8af031a09e50ad198670216b66b79c516e6938
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-05-24 21:51:27 +0200

    dokończone zadanie

--- UkladKartezianski/app.py
+++ UkladKartezianski/app.py
@@ -1,23 +1,31 @@
-class startPoint:
-    x = 0
-    y = 0
 
+def bubbleSort(pointsArr, ilosc):
 
-p1 = startPoint()
+    for i in range(ilosc):
+        for j in range(0, ilosc-i-1):
+            if pointsArr[j][2] > pointsArr[j+1][2]:
+                pointsArr[j], pointsArr[j+1] = pointsArr[j+1], pointsArr[j]
+    print('Wspolrzedne po sortowaniu: \n', pointsArr)
+
 
 with open('punkty.txt', encoding='utf8') as tekst:
     ilosc = tekst.readline()
     ilosc = int(ilosc)
     lines = []
-
-    print(lines)
+    pointsArr = []
 
     for x in range(ilosc):
-        lines[[x]].append(tekst.readline().replace(
-            "\t", " ").replace("\n", ""))
+        lines.append(tekst.readline().replace(
+            "\t", "|").replace("\n", ""))
+        pointsArr.append(lines[x].split("|"))
 
+i = 0
 
-tekst.close()
-print(lines)
+for x in range(ilosc):
+    for y in range(2):
+        pointsArr[x][y] = int(pointsArr[x][y])
 
-print(lines[0])
+    pointsArr[x].append(float(round(pointsArr[x][i]/pointsArr[x][i+1], 4)))
+
+print('Wspolrzedne przed sortowaniem: \n', pointsArr)
+bubbleSort(pointsArr, ilosc)
